b5e33a21dc768c2ad2206e0efe8559c04cb8aac5
kaviya.sellamuthu <kaviya.sellamuthu@accenture.com>
2025-09-22 21:48:32 +0530

    Prepare MERN app for Railway deployment

--- backend/server.js
+++ backend/server.js
@@ -4,24 +4,30 @@ import cors from "cors";
 import dotenv from "dotenv";
 import authRoutes from "./routes/authRoutes.js";
 import adminRoutes from "./routes/adminRoutes.js";
-import leaveRoutes from "./routes/leaveRoutes.js"; // ✅ import leave routes
+import leaveRoutes from "./routes/leaveRoutes.js"; 
+import path from "path"
 
 dotenv.config();
 const app = express();
 
 app.use(express.json());
 
-// ✅ Fix CORS
+// Serve static frontend
+app.use(express.static(path.join(__dirname, "frontend/build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend/build", "index.html"));
+});
 app.use(cors({
   origin: "http://localhost:5173",
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
 }));
 
-// ✅ Routes
+
 app.use("/api/auth", authRoutes);
 app.use("/api/admin", adminRoutes);
-app.use("/api/leave", leaveRoutes); // ✅ register leave routes
+app.use("/api/leave", leaveRoutes);
 
 const PORT = process.env.PORT || 5000;
 mongoose.connect(process.env.MONGO_URI, {
